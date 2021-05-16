ff8e1873f3df8b987e56b714e704631f1f4ee971
Dmitriy <kiverinda@yandex.ru>
2021-05-16 18:15:31 +0500

    Homework_3

---
+++ Timesheets/Domain/Interfaces/IPersonsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Timesheets.Models;
+using Timesheets.Models.Dto;
+using Person = Timesheets.Models.Person;
+
+namespace Timesheets.Domain.Interfaces
+{
+    public interface IPersonsManager
+    {
+        Person GetItem(int id);
+        List<Person> GetItemByName(string name);
+        List<Person> GetAll(int skip, int take);
+        int Create(PersonDto item);
+        void Update(Person item);
+        void Delete(int id);
+    }
+}
---
+++ Timesheets/IPersonsDb.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Timesheets.Models;
+
+namespace Timesheets
+{
+    public interface IPersonsDb
+    {
+        List<Person> Persons { get; set; }
+        int GetCount();
+    }
+}
